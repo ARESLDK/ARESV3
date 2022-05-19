6ef6138dc83ea1273860cb1cf5482f3b0432a56d
ARESLDK <104049345+ARESLDK@users.noreply.github.com>
2022-05-18 21:54:19 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 global.DeveloperMode = 'false' //true Or false
-global.linkGC = ['https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ' , 'https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs' , 'https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F' , 'https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC' , 'https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb'] // No tiene utilidad 
+global.linkGC = ['https://chat.whatsapp.com/FfWCBZMB8BQ0QGlPGsLAnr' , 'https://chat.whatsapp.com/DGMQBejz1S229wBKTabu4p' , 'https://chat.whatsapp.com/IEeWbt18rLF1KM3View7l2'] // No tiene utilidad 
 global.channelYT = ['https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'] // No tiene utilidad
 global.owner = ['526566070070'] //Cambia los numeros por tu o tus numeros a los cuales te comtactaran y gestionaras el Bot
 global.mods = ['526566070070'] //No tiene utilidad
