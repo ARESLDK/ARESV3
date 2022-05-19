1823d5878b6d1366a3de07d89d649f2fc6338698
ARESLDK <104049345+ARESLDK@users.noreply.github.com>
2022-05-19 14:51:01 -0600

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 function handler(m) {
 this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-this.sendContact(m.chat, '5219992095479', 'Shadow - Creador - Oficial 1', m)
-this.sendContact(m.chat, '17722386341', 'Shadow - Creador - Oficial 2', m)}
+this.sendContact(m.chat, '526566070070', '© ƛ  Ʀ  Є  Ƨ  🕷️⃟⃥✞︎⸸³³³1', m)
+this.sendContact(m.chat, '855384407990', '𒈞 ƛ Ʀ Є Ƨ 𒈞', m)}
 handler.command = /^(contacto|owner|creator|creador|propietario|dueño)$/i
 module.exports = handler
