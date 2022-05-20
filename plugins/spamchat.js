35f839b966a2c7844a6e3761c1cdda3ec2cfe213
ARESLDK <104049345+ARESLDK@users.noreply.github.com>
2022-05-20 16:56:28 -0600

    Update spamchat.js

--- plugins/spamchat.js
+++ plugins/spamchat.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, '*_Ingrese el texto que se enviará como spam!_*', m)
 
     let pesan = `${text}`
-    await m.reply('*_INICIO DE SPAM!_*\n\n*_Nota: El Bot enviará el mensaje 30 veces_*')
+    await m.reply('*_INICIO DE SPAM!_*\n\n*_Nota: 乂 𝐀𝐑𝐄𝐒 乂 enviará el mensaje 30 veces_*')
     await m.reply(pesan)
     await m.reply(pesan)
     await m.reply(pesan)
